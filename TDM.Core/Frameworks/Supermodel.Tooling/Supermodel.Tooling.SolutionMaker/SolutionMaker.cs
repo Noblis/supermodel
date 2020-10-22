e2220903053ec73306929bd977154fa04b05373c
ibasin <ilya.basin@gmail.com>
2020-10-21 22:15:30 -0400

    progress

--- TDM.Core/Frameworks/Supermodel.Tooling/Supermodel.Tooling.SolutionMaker/SolutionMaker.cs
+++ TDM.Core/Frameworks/Supermodel.Tooling/Supermodel.Tooling.SolutionMaker/SolutionMaker.cs
@@ -80,11 +80,11 @@ namespace Supermodel.Tooling.SolutionMaker
             {
                 //Droid
                 File.Delete(Path.Combine(path, @"XXYXX\Mobile\XXYXX.Mobile.Droid\MainActivity.cs"));
-                File.Move(Path.Combine(path, @"XXYXX\Mobile\XXYXX.Mobile.Droid\MainActivity.XamarinForms.cs"), Path.Combine(path, @"\XXYXX\Mobile\XXYXX.Mobile.Droid\MainActivity.cs"));
+                File.Move(Path.Combine(path, @"XXYXX\Mobile\XXYXX.Mobile.Droid\MainActivity.XamarinForms.cs"), Path.Combine(path, @"XXYXX\Mobile\XXYXX.Mobile.Droid\MainActivity.cs"));
 
                 //iOS
                 File.Delete(Path.Combine(path, @"XXYXX\Mobile\XXYXX.Mobile.iOS\AppDelegate.cs"));
-                File.Move(Path.Combine(path, @"XXYXX\Mobile\XXYXX.Mobile.iOS\AppDelegate.XamarinForms.cs"), Path.Combine(path, @"\XXYXX\Mobile\XXYXX.Mobile.iOS\AppDelegate.cs"));
+                File.Move(Path.Combine(path, @"XXYXX\Mobile\XXYXX.Mobile.iOS\AppDelegate.XamarinForms.cs"), Path.Combine(path, @"XXYXX\Mobile\XXYXX.Mobile.iOS\AppDelegate.cs"));
             }
             else
             {
