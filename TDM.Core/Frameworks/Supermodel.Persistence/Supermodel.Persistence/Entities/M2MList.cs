273e6f606da79371511379fcb7488078f00833e4
ibasin <ilya.basin@gmail.com>
2020-11-23 11:44:59 -0500

    progress

--- TDM.Core/Frameworks/Supermodel.Persistence/Supermodel.Persistence/Entities/M2MList.cs
+++ TDM.Core/Frameworks/Supermodel.Persistence/Supermodel.Persistence/Entities/M2MList.cs
@@ -4,16 +4,5 @@ using System.Collections.Generic;
 
 namespace Supermodel.Persistence.Entities
 {
-    public class M2MTo1Lis2<TM2M> : M2MList<TM2M> where TM2M : IM2M
-    {
-
-    }
-    public class M2MTo1List<TM2M> : M2MList<TM2M> where TM2M : IM2M
-    {
-
-    }
-    
-    public abstract class M2MList<TM2M> : List<TM2M> where TM2M : IM2M
-    {
-    }
+    public class M2MList<TM2M> : List<TM2M> where TM2M : IM2M { }
 }
