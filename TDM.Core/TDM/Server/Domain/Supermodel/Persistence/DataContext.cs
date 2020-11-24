dfdf8243d53abf1aa6354f1ce8d0c2b0ba39f336
ibasin <ilya.basin@gmail.com>
2020-11-23 20:15:32 -0500

    progress

--- TDM.Core/TDM/Server/Domain/Supermodel/Persistence/DataContext.cs
+++ TDM.Core/TDM/Server/Domain/Supermodel/Persistence/DataContext.cs
@@ -1,6 +1,5 @@
 ﻿#nullable enable
 
-using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 //using Supermodel.Persistence.EFCore.SQLServer;
 using Supermodel.Persistence.EFCore.SQLite;
@@ -10,10 +9,7 @@ namespace Domain.Supermodel.Persistence
     public class DataContext : EFCoreSQLiteDataContext
     {
         #region Constructors
-        public DataContext() 
-            : base(RuntimeInformation.IsOSPlatform(OSPlatform.Windows)?
-                @"Filename=/Users/ilyabasin/Documents/Projects/TDM.Core/TDM/Server/TDMCore.SQLite.db" : 
-                @"Filename=/Users/m29598/tdm.core/TDM/Server/TDMCore.SQLite.db") { }
+        public DataContext() : base($"Filename={DbFilePath}TDMCore.SQLite.db") { }
         #endregion
 
         #region Overrides
--- TDM.Core/TDM/Server/WMDomain/Supermodel/Persistence/DataContext.cs
+++ TDM.Core/TDM/Server/WMDomain/Supermodel/Persistence/DataContext.cs
@@ -1,7 +1,6 @@
 ﻿#nullable enable
 
 using Supermodel.Persistence.EFCore.SQLite;
-using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 //using Supermodel.Persistence.EFCore.SQLServer;
 
@@ -10,10 +9,7 @@ namespace WMDomain.Supermodel.Persistence
     public class DataContext : EFCoreSQLiteDataContext
     {
         #region Constructors
-        public DataContext() 
-            : base(RuntimeInformation.IsOSPlatform(OSPlatform.Windows)?
-                @"Filename=/Users/ilyabasin/Documents/Projects/TDM.Core/TDM/Server/TDMCore.SQLite.db" : 
-                @"Filename=/Users/m29598/tdm.core/TDM/Server/TDMCore.SQLite.db") { }
+        public DataContext() : base($"Filename={DbFilePath}TDMCore.SQLite.db") { }
         #endregion
 
         #region Overrides
