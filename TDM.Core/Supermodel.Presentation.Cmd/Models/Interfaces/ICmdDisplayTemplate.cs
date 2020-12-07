7698629ffd9153d647861879a427d4b0675e8607
ibasin <ilya.basin@gmail.com>
2020-12-07 17:10:13 -0500

    progress

--- TDM.Core/Supermodel.Presentation.Cmd/Models/Interfaces/ICmdDisplayTemplate.cs
+++ TDM.Core/Supermodel.Presentation.Cmd/Models/Interfaces/ICmdDisplayTemplate.cs
@@ -1,8 +1,11 @@
-﻿#nullable enable
+﻿using Supermodel.Presentation.Cmd.ConsoleOutput;
+
+#nullable enable
 
 namespace Supermodel.Presentation.Cmd.Models.Interfaces
 {
     interface ICmdDisplayTemplate
     {
+        IConsoleOutput DisplayTemplate<TModel>(int screenOrderFrom = int.MinValue, int screenOrderTo = int.MaxValue);    
     }
 }
--- TDM.Core/Supermodel.Presentation.Cmd/Models/Interfaces/ICmdEditorTemplate.cs
+++ TDM.Core/Supermodel.Presentation.Cmd/Models/Interfaces/ICmdEditorTemplate.cs
@@ -1,8 +1,11 @@
 ﻿#nullable enable
 
+using Supermodel.Presentation.Cmd.ConsoleOutput;
+
 namespace Supermodel.Presentation.Cmd.Models.Interfaces
 {
     interface ICmdEditorTemplate
     {
+        IConsoleOutput EditorTemplate<TModel>(int screenOrderFrom = int.MinValue, int screenOrderTo = int.MaxValue);    
     }
 }
