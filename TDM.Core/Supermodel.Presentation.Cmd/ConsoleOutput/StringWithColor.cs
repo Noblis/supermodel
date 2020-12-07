12df0554f44d6ab5f75f407f1875ad8945ae29af
ibasin <ilya.basin@gmail.com>
2020-12-07 18:37:28 -0500

    progress

--- TDM.Core/Supermodel.Presentation.Cmd/ConsoleOutput/StringWithColor.cs
+++ TDM.Core/Supermodel.Presentation.Cmd/ConsoleOutput/StringWithColor.cs
@@ -4,9 +4,85 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
+using System.Text;
 
 namespace Supermodel.Presentation.Cmd.ConsoleOutput
 {
+    public class StringBuilderWithColor : IConsoleOutput
+    {
+        #region Constructors
+        public StringBuilderWithColor(StringWithColor str)
+        {
+            Content = new StringBuilder(str.Content);
+            ColorChanges = str.ColorChanges.ToList();
+        }
+        #endregion
+
+        #region Overrides
+        public override string ToString()
+        {
+            return Content.ToString();
+        }
+        #endregion
+
+        #region Methods
+        public void Append(string str)
+        {
+            Content.Append(str);
+        }
+        public void AppendLine(string str)
+        {
+            Content.AppendLine(str);
+        }
+        public void Append(StringWithColor str)
+        {
+            Content.Append(str.Content);
+            AppendColorChanges(str.ColorChanges);
+        }
+        public void AppendLine(StringWithColor str)
+        {
+            Content.AppendLine(str.Content);
+            AppendColorChanges(str.ColorChanges);
+        }
+        protected void AppendColorChanges(ImmutableArray<ColorChange> colorChanges)
+        {
+            var offset = Content.Length;
+            foreach (var colorChange in colorChanges) 
+            {
+                if (ColorChanges.Count == 0 || ColorChanges.Last().Colors != colorChange.Colors) ColorChanges.Add(colorChange.CloneWithOffset(offset));
+            }
+        }
+        #endregion
+
+        #region IConsoleOutput
+        public virtual void WriteToConsole(bool writeLine = true)
+        {
+            var content = Content.ToString();
+            var currentColorChange = new ColorChange(0, null, null);
+            
+            foreach (var colorChange in ColorChanges)
+            {
+                var strPortion = content[currentColorChange.Index..colorChange.Index];
+                currentColorChange.Colors.SetColors();
+                Console.Write(strPortion);
+
+                currentColorChange = colorChange;
+            }
+            
+            var strEndPortion = content[currentColorChange.Index..];
+            currentColorChange.Colors.SetColors();
+            Console.Write(strEndPortion);
+
+            if (writeLine) Console.WriteLine();
+        }
+        #endregion
+        
+        #region Properties
+        public StringBuilder Content { get; }
+        public List<ColorChange> ColorChanges { get; }
+        #endregion
+    }
+    
     public class StringWithColor : IConsoleOutput
     {
         #region Constructors
@@ -24,6 +100,13 @@ namespace Supermodel.Presentation.Cmd.ConsoleOutput
         {
             Content = content;
             ColorChanges = colorChanges.ToImmutableArray();
+        }
+        #endregion
+
+        #region Overrides
+        public override string ToString()
+        {
+            return Content;
         }
         #endregion
 
--- TDM.Core/Supermodel.Presentation.Cmd/Models/CmdModel.cs
+++ TDM.Core/Supermodel.Presentation.Cmd/Models/CmdModel.cs
@@ -1,12 +1,79 @@
-﻿using System;
+﻿#nullable enable
+
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
+using Supermodel.Presentation.Cmd.ConsoleOutput;
+using Supermodel.Presentation.Cmd.Models.Interfaces;
 
 namespace Supermodel.Presentation.Cmd.Models
 {
-    class CmdModel
+    public class CmdModel : ICmdModel
     {
+        public virtual IConsoleOutput EditorTemplate<TModel>(int screenOrderFrom = int.MinValue, int screenOrderTo = int.MaxValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual IConsoleOutput DisplayTemplate<TModel>(int screenOrderFrom = int.MinValue, int screenOrderTo = int.MaxValue)
+        {
+            var result = new StringBuilderWithColor();
+            foreach (var propertyInfo in GetType().GetDetailPropertyInfosInOrder(screenOrderFrom, screenOrderTo))
+            {
+                //Div 1
+                var propMarkerAttribute = markerAttribute;
+                var htmlAttrAttribute = propertyInfo.GetAttribute<HtmlAttrAttribute>();
+                if (htmlAttrAttribute != null) propMarkerAttribute += " " + htmlAttrAttribute.Attr;
+                result.AppendLine("<div class='form-group row'" + propMarkerAttribute + " >"); 
+
+                //Label
+                var hideLabelAttribute = propertyInfo.GetAttribute<HideLabelAttribute>();
+                if (hideLabelAttribute == null)
+                {
+                    var labelHtml = html.Super().Label(propertyInfo.Name, new { @class = ScaffoldingSettings.DisplayLabelCssClass }).GetString();
+                    if (!propertyInfo.HasAttribute<NoRequiredLabelAttribute>())
+                    {
+                        if (propertyInfo.HasAttribute<RequiredAttribute>() || propertyInfo.HasAttribute<ForceRequiredLabelAttribute>()) labelHtml = labelHtml.Replace("</label>", $"<sup><em class='text-danger font-weight-bold {ScaffoldingSettings.RequiredAsteriskCssClass}'>*</em></sup></label>", true, CultureInfo.InvariantCulture);
+                    }
+                    result.AppendLine(labelHtml);
+                }
+                else
+                {
+                    if (hideLabelAttribute.KeepLabelSpace) result.AppendLine($"<div {UtilsLib.MakeClassAttribute(ScaffoldingSettings.DisplayLabelCssClass)}></div>");
+                }
+                
+                //Div 2
+                if (hideLabelAttribute == null || hideLabelAttribute.KeepLabelSpace) result.AppendLine("<div class='col-sm-10'>");
+                else result.AppendLine("<div class='col-sm-12'>");
+
+                //Value
+                result.AppendLine("<span " + UtilsLib.MakeClassAttribute(ScaffoldingSettings.DisplayCssClass) + ">");
+                result.AppendLine(html.Super().Display(propertyInfo.Name).GetString());
+                result.AppendLine("</span>");
+                
+                result.AppendLine("</div>"); //close Div 2
+                result.AppendLine("</div>"); //close Div 1
+            }
+            return result.ToHtmlString();         
+        }
+
+        private IEnumerable<PropertyInfo> GetDetailPropertyInfosInOrder(int screenOrderFrom = int.MinValue, int screenOrderTo = int.MaxValue)
+        {
+            var result = GetType().GetProperties()
+                .Where(x => x.GetCustomAttribute<ScaffoldColumnAttribute>() == null || x.GetCustomAttribute<ScaffoldColumnAttribute>()!.Scaffold)
+                .Where(x => (x.GetCustomAttribute<ScreenOrderAttribute>() != null ? x.GetCustomAttribute<ScreenOrderAttribute>()!.Order : 100) >= screenOrderFrom)
+                .Where(x => (x.GetCustomAttribute<ScreenOrderAttribute>() != null ? x.GetCustomAttribute<ScreenOrderAttribute>()!.Order : 100) <= screenOrderTo)
+                .OrderBy(x => x.GetCustomAttribute<ScreenOrderAttribute>() != null ? x.GetCustomAttribute<ScreenOrderAttribute>()!.Order : 100);
+        
+            //By default we do not scaffold enumerations (except for strings of course and unless they implement ISupermodelEditorTemplate)
+            return result.Where(x => !(x.PropertyType != typeof(string) && 
+                                       !typeof(ISupermodelEditorTemplate).IsAssignableFrom(x.PropertyType) && 
+                                       typeof(IEnumerable).IsAssignableFrom(x.PropertyType)));
+        }
+
     }
 }
