f1c4c19e62bdeb971f59b64e70edb9ccf58a218b
ibasin <ilya.basin@gmail.com>
2020-12-14 18:58:31 -0500

    progress

--- TDM.Core/Frameworks/Supermodel.Presentation/Cmd/Supermodel.Presentation.Cmd/Models/CmdContext.cs
+++ TDM.Core/Frameworks/Supermodel.Presentation/Cmd/Supermodel.Presentation.Cmd/Models/CmdContext.cs
@@ -1,7 +1,6 @@
 ﻿#nullable enable
 
 using System;
-using System.Threading;
 using Supermodel.DataAnnotations.Validations;
 
 namespace Supermodel.Presentation.Cmd.Models
@@ -46,36 +45,43 @@ namespace Supermodel.Presentation.Cmd.Models
         #endregion
 
         #region Properties
-        public static ValidationResultList ValidationResultList 
-        { 
-            get 
-            {
-                _validationResultList.Value ??= new ValidationResultList();
-                return _validationResultList.Value;
-            }
-        }
-        private static readonly AsyncLocal<ValidationResultList> _validationResultList = new AsyncLocal<ValidationResultList> { Value = new ValidationResultList() };
-
-        public static string? PropertyDisplayName
-        {
-            get => _propertyDisplayName.Value!;
-            set => _propertyDisplayName.Value = value;
-        }
-        private static readonly AsyncLocal<string?> _propertyDisplayName = new AsyncLocal<string?>();
-
-        public static bool IsPropertyRequired
-        {
-            get => _isPropertyRequired.Value!;
-            set => _isPropertyRequired.Value = value;
-        }
-        private static readonly AsyncLocal<bool> _isPropertyRequired = new AsyncLocal<bool>();
+        public static ValidationResultList ValidationResultList { get; set; } = new ValidationResultList();
         
-        public static bool CtrlEscEnabled
-        {
-            get => _ctrlEscEnabled.Value!;
-            set => _ctrlEscEnabled.Value = value;
-        }
-        private static readonly AsyncLocal<bool> _ctrlEscEnabled = new AsyncLocal<bool>();
+        public static string? PropertyDisplayName { get; set; }
+        public static bool IsPropertyRequired { get; set; }
+
+        public static bool CtrlEscEnabled { get; set; }
+        
+        //public static ValidationResultList ValidationResultList 
+        //{ 
+        //    get 
+        //    {
+        //        _validationResultList.Value ??= new ValidationResultList();
+        //        return _validationResultList.Value;
+        //    }
+        //}
+        //private static readonly AsyncLocal<ValidationResultList> _validationResultList = new AsyncLocal<ValidationResultList> { Value = new ValidationResultList() };
+
+        //public static string? PropertyDisplayName
+        //{
+        //    get => _propertyDisplayName.Value!;
+        //    set => _propertyDisplayName.Value = value;
+        //}
+        //private static readonly AsyncLocal<string?> _propertyDisplayName = new AsyncLocal<string?>();
+
+        //public static bool IsPropertyRequired
+        //{
+        //    get => _isPropertyRequired.Value!;
+        //    set => _isPropertyRequired.Value = value;
+        //}
+        //private static readonly AsyncLocal<bool> _isPropertyRequired = new AsyncLocal<bool>();
+        
+        //public static bool CtrlEscEnabled
+        //{
+        //    get => _ctrlEscEnabled.Value!;
+        //    set => _ctrlEscEnabled.Value = value;
+        //}
+        //private static readonly AsyncLocal<bool> _ctrlEscEnabled = new AsyncLocal<bool>();
 
         // ReSharper disable once UnusedAutoPropertyAccessor.Local
         private static RequiredState CurrentRequiredState { get; set; } //this is to store it away from GC
