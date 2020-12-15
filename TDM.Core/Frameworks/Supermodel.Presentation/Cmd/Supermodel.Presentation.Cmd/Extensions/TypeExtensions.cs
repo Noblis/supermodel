90561c40c90b6c20f9441e2c322f9f57c439aad2
ibasin <ilya.basin@gmail.com>
2020-12-15 17:38:28 -0500

    progress

---
+++ TDM.Core/Frameworks/Supermodel.Presentation/Cmd/Supermodel.Presentation.Cmd/Extensions/TypeExtensions.cs
@@ -0,0 +1,23 @@
+﻿#nullable enable
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using Supermodel.DataAnnotations.Attributes;
+using Supermodel.Persistence.Entities;
+
+namespace Supermodel.Presentation.Cmd.Extensions
+{
+    public static class TypeExtensions
+    {
+        #region Methods
+        public static bool IsEntityType(this Type me)
+        {
+            return typeof (IEntity).IsAssignableFrom(me);
+        }
+        #endregion
+    }
+}
--- TDM.Core/Frameworks/Supermodel.Presentation/Cmd/Supermodel.Presentation.Cmd/Models/Base/SingleSelectCmdModel.cs
+++ TDM.Core/Frameworks/Supermodel.Presentation/Cmd/Supermodel.Presentation.Cmd/Models/Base/SingleSelectCmdModel.cs
@@ -3,9 +3,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection.Emit;
-using Supermodel.DataAnnotations.Misc;
-using Supermodel.Presentation.Cmd.ConsoleOutput;
 
 namespace Supermodel.Presentation.Cmd.Models.Base
 {
@@ -28,57 +25,24 @@ namespace Supermodel.Presentation.Cmd.Models.Base
         #endregion
 
         #region Static Dropdown and Radio helpers
-        public virtual IGenerateHtml CommonDropdownEditorTemplate(SingleSelectMvcModel singleSelect, AttributesDict? attributesDict)
-        {
-            attributesDict ??= new AttributesDict();
-            attributesDict.AddOrAppendCssClass("form-control");
+        //public virtual object? CommonDropdownEditorTemplate(SingleSelectMvcModel singleSelect, AttributesDict? attributesDict)
+        //{
+        //    attributesDict ??= new AttributesDict();
+        //    attributesDict.AddOrAppendCssClass("form-control");
 
-            var selectListItemList = new List<ConsoleExt.SelectListItem> { ConsoleExt.SelectListItem.Empty };
-            foreach (var option in singleSelect.Options)
-            {
-                var isSelectedOption = singleSelect.SelectedValue != null && string.CompareOrdinal(singleSelect.SelectedValue, option.Value) == 0;
-                if (isSelectedOption || !option.IsDisabled)
-                {
-                    var item = new ConsoleExt.SelectListItem(option.Value, !option.IsDisabled ? option.Label : option.Label + DisabledSuffix);
-                    selectListItemList.Add(item);
-                }
-            }
-            var result = Render.DropdownListForModel(singleSelect.SelectedValue, selectListItemList, attributesDict);
-            return result;
-        }
-        public virtual IGenerateHtml CommonRadioEditorTemplate(SingleSelectMvcModel singleSelect, AttributesDict? divAttributesDict, AttributesDict? inputAttributesDict, AttributesDict? labelAttributesDict)
-        {
-            var prefix = HttpContext.Current.PrefixManager.CurrentPrefix;
-            var id = prefix.ToHtmlId();
-            var name = prefix.ToHtmlName();
-
-            var result = new HtmlStack();
-
-            //Set up class attributes
-            divAttributesDict ??= new AttributesDict();
-            divAttributesDict.AddOrAppendCssClass("form-check");
-
-            inputAttributesDict ??= new AttributesDict();
-            inputAttributesDict.AddOrAppendCssClass("form-check-input");
-
-            labelAttributesDict ??= new AttributesDict();
-            labelAttributesDict.AddOrAppendCssClass("form-check-label");
-
-            foreach (var option in singleSelect.Options)
-            {
-                result.AppendAndPush(new Div(divAttributesDict));
-                var isSelectedOption = singleSelect.SelectedValue != null && string.CompareOrdinal(singleSelect.SelectedValue, option.Value) == 0;
-                if (isSelectedOption || !option.IsDisabled)
-                {
-                    result.Append(Render.RadioButtonForModel(singleSelect.SelectedValue, option.Value, inputAttributesDict));
-                    result.Append(new Label(new { @for = name }) { new Txt(!option.IsDisabled ? option.Label : option.Label + DisabledSuffix) }).AddOrUpdateAttr(labelAttributesDict);
-                }
-                result.Pop<Div>();
-            }
-            result.Append(new Input(new { id, name, type="hidden", value="" }));
-
-            return result;
-        }
+        //    var selectListItemList = new List<ConsoleExt.SelectListItem> { ConsoleExt.SelectListItem.Empty };
+        //    foreach (var option in singleSelect.Options)
+        //    {
+        //        var isSelectedOption = singleSelect.SelectedValue != null && string.CompareOrdinal(singleSelect.SelectedValue, option.Value) == 0;
+        //        if (isSelectedOption || !option.IsDisabled)
+        //        {
+        //            var item = new ConsoleExt.SelectListItem(option.Value, !option.IsDisabled ? option.Label : option.Label + DisabledSuffix);
+        //            selectListItemList.Add(item);
+        //        }
+        //    }
+        //    var result = Render.DropdownListForModel(singleSelect.SelectedValue, selectListItemList, attributesDict);
+        //    return result;
+        //}
         #endregion
 
         #region IUIComponentWithValue implemetation
