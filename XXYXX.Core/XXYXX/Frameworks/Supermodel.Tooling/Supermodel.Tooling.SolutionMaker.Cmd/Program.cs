a4fd1a78797e4541915329439f7ec709373c2526
Ilya Basin <ilya.basin@gmail.com>
2021-03-24 17:38:56 -0400

    progress

--- XXYXX.Core/XXYXX/Frameworks/Supermodel.Tooling/Supermodel.Tooling.SolutionMaker.Cmd/Program.cs
+++ XXYXX.Core/XXYXX/Frameworks/Supermodel.Tooling/Supermodel.Tooling.SolutionMaker.Cmd/Program.cs
@@ -10,7 +10,7 @@ namespace Supermodel.Tooling.SolutionMaker.Cmd
         {
             try
             {
-                SolutionMaker.Version = "2.05.000.Beta3";
+                SolutionMaker.Version = "2.06.000.Beta1";
 
                 ////*******Un-comment and run this once to refresh the solution zip
                 Console.WriteLine($"v{SolutionMaker.Version}");
