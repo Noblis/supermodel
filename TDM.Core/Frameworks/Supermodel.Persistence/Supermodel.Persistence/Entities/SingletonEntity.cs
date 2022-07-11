abd6cfb28fb7a02d4bbf75ed78552500d4dd13ae
Ilya Basin <ilya.basin@gmail.com>
2022-07-11 15:38:22 -0400

    Added SingletonEntity

---
+++ TDM.Core/Frameworks/Supermodel.Persistence/Supermodel.Persistence/Entities/SingletonEntity.cs
@@ -0,0 +1,27 @@
+﻿#nullable enable
+
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using Supermodel.DataAnnotations.Validations;
+
+namespace Supermodel.Persistence.Entities
+{
+    public abstract class SingletonEntity : Entity
+    {
+        #region Overrides
+        protected override void DeleteInternal()
+        {
+            throw new UnableToDeleteException("Settings cannot be deleted");
+        }
+        #endregion
+
+        #region Validation
+        public override async Task<ValidationResultList> ValidateAsync(ValidationContext validationContext)
+        {
+            var vr = await base.ValidateAsync(validationContext);
+            if (Id != 1 && Id != 0) vr.Add(new ValidationResult("There could only be one record in Settings table"));
+            return vr;
+        }
+        #endregion
+    }
+}
